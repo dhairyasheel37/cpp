7cabfc26d4513ca044c65c4ab10b7d277717529b
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-08-07 23:08:38 +0530

    Add files via upload

---
+++ data_types.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    short s=2;
+    int l=30;
+    int a=100;
+    long long1;
+    long long ll=1000;
+    float f=45.32;
+    cout<<"The score is "<<f<<endl;
+    cout<<"a+l = "<<a+l<<endl;
+    cout<<"a/l = "<<(float)a/l<<endl;
+}
