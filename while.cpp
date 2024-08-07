b1a8210bcc4a283fed18fe58cea01643936c6951
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-08-07 23:13:00 +0530

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0;
+    while(i<=10)
+    {
+        //print 1 to 10 numbers
+        cout<<"We are at index number :"<<i<<endl;
+        i++;
+    }
+}
