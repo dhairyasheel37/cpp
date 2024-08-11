39fed724b9499e1e6f145b2a6d7b4c3bb908b5d4
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-08-11 23:08:53 +0530

    Create datatypes_hackerrank.c

---
+++ datatypes_hackerrank.c
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+
+    int a;
+    long ld;
+    char c;
+    float f;
+    double d;
+    cin>>a;
+    cin>> ld;
+    cin>> c;
+    cin>>f;
+    cin>>d;
+
+    printf("%d\n%ld\n%c\n%f\n%lf",a,ld,c,f,d);
+    return 0;
+}
