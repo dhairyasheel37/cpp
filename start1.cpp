b7a9bd2a9199ac99f7690c337591141d993996b8
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-10-19 23:45:12 +0530

    Create start1.cpp

---
+++ start1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Include input-output stream library
+
+int main() {
+    std::cout << "Hello, World!";  // Output "Hello, World!" to the console
+    return 0;  // End of the program
+}
