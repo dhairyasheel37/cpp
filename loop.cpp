dc96c100822ad3c84f2f803805540de57a8b48c4
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-10-24 22:24:25 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    //for loop
+    // for(i=1;i<=10;i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // table of 6
+    // cout<<"table of 6"<<endl;
+    // for(i=0;i<=10;i++)
+    // {
+    //     cout<<6*i<<endl;
+    // }
+
+
+    
+    
+
+    //while loop
+    // 
+    // while(i<=10)
+    // {
+    //     cout<< i<<endl;
+    //     i++;
+    // }
+
+    // while (i<=10)
+    // {
+    //     cout<<7*i<<endl;
+    //     i++;
+    //     /* code */
+    // }
+
+
+
+
+
+    //do while
+    // do
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    //     /* code */
+    // }while (i<=10);
+    
+    do
+    {
+        cout<<8*i<<endl;
+        i++;
+        /* code */
+    }while (i<=10);
+
+    
+
+}
