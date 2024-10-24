866306c929fd7ce9dffda8c755d074fdd787fcc6
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-10-24 22:22:36 +0530

    basic of c++

---
+++ qw.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int c = 15;// globale variable
+int main()
+{
+    // int a = 4;
+    // int b = 5;
+    // float pi = 3.14;
+    // char c ='d';
+    // cout<<"The value of a is "<<a<<".The value of b is "<<b<<"."<<endl;
+    // cout<<"The value of pi is "<<pi<<"."<<endl;
+    // cout<<"t=The character is "<<c<<"."<<endl;
+
+
+
+    // int num1, num2;
+    // cout<<"Enter the value of num1 :";
+    // cin>>num1;
+    // cout<<"Enter the value of num2 :";
+    // cin>>num2;
+    // cout<<"The sum is "<<num1+num2<<endl;
+
+
+    
+
+
+
+    // tut 7   Reference Variables & Typecasting
+    // int a,b,c;
+    // cout<<"Enter the value  of a : "<<endl;
+    // cin>>a;
+    // cout<<"Enter the value  of b : "<<endl;
+    // cin>>b;
+    // c = a+b;
+    // cout<<"The sum  is : "<<c<<endl; 
+    // cout<<"The global value is : "<<::c<<endl; 
+
+    // float d = 3.14f;
+    // long double e = 3.14l;
+    // cout <<"tThe value of d is :"<<d<<endl;
+    // cout <<"tThe value of e is :"<<e<<endl;
+
+    // float x = 10;
+    // float &y = x;
+    // cout << x << endl;
+    // cout<< y << endl;
+
+
+    // type casting
+    // int a = 45;
+    // float b = 45.46;
+    // cout<<"The value of a is :"<<(int)a<<endl;
+    // cout<<"The value of b is :"<<(float)b<<endl;
+    // int c = int(b);
+
+    // cout<<"The expression is "<<a+b<<endl;
+    // cout<<"The expression is "<<a+ int (b)<<endl;
+    // cout<<"The expression is "<<a+ (int) b<<endl;
+    
+
+// int a = 34;
+// cout << "The value of  a was: "<<a<<endl;
+
+// a=45;
+// cout << "The value of  a was: "<<a<<endl;
+ 
+//constants 
+    // const int a = 34;
+    // cout << "The value of  a was: "<<a<<endl;
+
+    // a=45; //This program show error Because of lable "const" the value of a doesn't not chang
+    // cout << "The value of  a was: "<<a<<endl;   
+
+//Manipulators 
+    int x = 7,y = 88,z = 7819;
+    cout<<"The of x is : "<<x<<endl;
+    cout<<"The of y is : "<<y<<endl;
+    cout<<"The of z is : "<<z<<endl;
+
+    cout<<"The of x with setw is : "<<setw(4)<<x<<endl;
+    cout<<"The of x with setw is : "<<setw(4)<<y<<endl;
+    cout<<"The of x with setw is : "<<setw(4)<<z<<endl;
+
+// Operator precedance
+    
+
+return 0;
+
+}
