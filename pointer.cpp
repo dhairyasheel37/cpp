7f80897e5cde5b512c596cfdc02c88fa4c3c7e02
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-10-24 22:25:47 +0530

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 3;
+    int* b=&a;
+    cout<<b<<endl;
+    cout<<*b<<endl;
+
+   int** c = &b;
+    cout<<c<<endl;
+    cout<<*c<<endl;
+    cout<<**c<<endl;
+
+
+    return 0;
+}
