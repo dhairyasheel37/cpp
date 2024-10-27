bf27627bfc1966a8e24445d3e097c8b72bae7e6c
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-10-27 21:43:05 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n<1)
+    return 1;
+    else
+    {
+        return n* factorial(n-1);
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<"Factiorial of "<<n<<" is "<<factorial(n)<<endl;
+    return 0;
+}
